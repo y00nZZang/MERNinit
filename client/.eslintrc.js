f874bef520ef38b934b8c1736f3cbec9a250c4d2
y00nZZang <84168285+y00nZZang@users.noreply.github.com>
2021-10-13 20:28:03 +0900

    style: client eslint, prettier 적용
    airbnb 스타일로 eslint, prettier 적용

---
+++ client/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['airbnb'],
+  rules: {
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
+    'linebreak-style': ['error', 'windows'],
+  },
+};
--- client/src/App.js
+++ client/src/App.js
@@ -3,25 +3,24 @@ import axios from 'axios';
 
 const client = axios.create();
 
-const App = ()=>{
+const App = () => {
   const [test, setTest] = useState('NOT CONNECTED');
 
   useEffect(() => {
-    async function fetchData(){
+    async function fetchData() {
       const res = await client.get('/test');
       setTest(res.data);
-    };
-    fetchData();
     }
-    , [])
+    fetchData();
+  }, []);
 
-  return(
+  return (
     <div>
       Hello World
-      <br/>
+      <br />
       {test}
     </div>
   );
-}
+};
 
 export default App;
